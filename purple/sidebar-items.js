cd7e4580e1564e14c767daee892158941be09084
syedazeez337 <syedazeez337@users.noreply.github.com>
2025-12-23 19:10:21 +0000

    deploy: 00a6ba69f074955fabbadb871e84c3cefe237715

--- purple/sidebar-items.js
+++ purple/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["main"],"macro":["filesystem_log","network_log","policy_log","sandbox_log","security_log"],"mod":["ai","cli","error","logging","policy","sandbox"]};
+window.SIDEBAR_ITEMS = {"fn":["main","validate_profile_name"],"macro":["filesystem_log","network_log","policy_log","sandbox_log","security_log"],"mod":["ai","cli","error","logging","policy","sandbox"]};
